85d6dc2f63e95830d285cc2f797735ca517dbbde
yarinsa <yarinsasson2@gmail.com>
2025-07-13 13:57:45 +0300

    add dotenv

--- client/apps/dashboard/vite.config.ts
+++ client/apps/dashboard/vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 // https://vite.dev/config/
 export default defineConfig(() => {
